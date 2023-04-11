fea3be3f572ad1b54b14e7c35046ddb97d96d93f
Mehmet Yiğit EROL <erolmeh@mef.edu.tr>
2023-04-11 18:23:40 +0300

    Pause, Restart and Exit
    Pause menu is added. Game can be restarted through the pause menu. Players can also exit the game through the pause menu.
    
    New "Paused" menu picture is added.

--- Tetris2048.java
+++ Tetris2048.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 // The main class to run the Tetris 2048 game
 public class Tetris2048 {
+   static boolean isRestarted = false;
    public static void main(String[] args) throws InterruptedException {
       // set the size of the game grid
       int gridH = 18, gridW = 12;
@@ -33,10 +34,13 @@ public class Tetris2048 {
       grid.setNextTetromino(nextTetromino);
       // display a simple menu before opening the game
       // by using the displayGameMenu method defined below
-      displayGameMenu(gridH, gridW);
+      if(isRestarted == false){
+         displayGameMenu(gridH, gridW);
+      }
       // the main game loop (using some keyboard keys for moving the tetromino)
       // -----------------------------------------------------------------------
       int iterationCount = 0; // used for the speed of the game
+      boolean isPaused = false;
       while (true) {
          // check user interactions via the keyboard
          // --------------------------------------------------------------------
@@ -67,8 +71,10 @@ public class Tetris2048 {
             currentTetromino.rotateCounterclockwise();
             Thread.sleep(20);
          }
-
-
+         else if (StdDraw.isKeyPressed(KeyEvent.VK_ESCAPE)){
+            StdDraw.pause(5);
+            displayPauseMenu(gridH, gridW);
+         }
 
          // move the active tetromino down by 1 once in 10 iterations (auto fall)
          boolean success = true;
@@ -159,5 +165,81 @@ public class Tetris2048 {
                   break; // break the loop to end the method and start the game
          }
       }
+   }
+
+   public static void displayPauseMenu(int gridHeight, int gridWidth) throws InterruptedException {
+      // colors used for the pause menu
+      Color backgroundColor = new Color(42, 69, 99);
+      Color buttonColor = new Color(25, 255, 228);
+      Color textColor = new Color(31, 160, 239);
+      // clear the background canvas to background_color
+      StdDraw.clear(backgroundColor);
+      // the relative path of the image file
+      String imgFile = "images/pauseMenu_image.png";
+      gridWidth = gridWidth + gridWidth / 4;
+      double imgCenterX = (gridWidth - 1) / 2.0, imgCenterY = gridHeight - 7;
+      StdDraw.picture(imgCenterX, imgCenterY + 2, imgFile);
+      double buttonW = gridWidth - 1.5, buttonH = 2;
+      double buttonX = imgCenterX, buttonY = 5;
+      StdDraw.setPenColor(buttonColor);
+
+      // creating the unpause button with +2.5 Y difference.
+      StdDraw.filledRectangle(buttonX, buttonY + 2.5, buttonW / 2, buttonH / 2);
+      // creating the restart button in the center.
+      StdDraw.filledRectangle(buttonX, buttonY, buttonW / 2, buttonH / 2);
+      // creating the exit game button with -2.5 Y difference.
+      StdDraw.filledRectangle(buttonX, buttonY - 2.5, buttonW / 2, buttonH / 2);
+
+      Font font = new Font("Arial", Font.PLAIN, 25);
+      StdDraw.setFont(font);
+      StdDraw.setPenColor(textColor);
+
+      String textToDisplay = "Unpause Game";
+      StdDraw.text(buttonX, buttonY + 2.5, textToDisplay);
+
+      textToDisplay = "Restart Game";
+      StdDraw.text(buttonX, buttonY, textToDisplay);
+
+      textToDisplay = "Exit Game";
+      StdDraw.text(buttonX, buttonY - 2.5, textToDisplay);
+
+      // menu interaction loop
+      while(true){
+         StdDraw.show();
+         StdDraw.pause(50);
+
+         // check if the mouse is clicked
+         if (StdDraw.isMousePressed()) {
+            // get the x and y coordinates of the position of the mouse
+            double mouseX = StdDraw.mouseX(), mouseY = StdDraw.mouseY();
+            // check if these coordinates are inside the button
+            if (mouseX > buttonX - buttonW / 2 && mouseX < buttonX + buttonW / 2)
+               // this if checks the coordinates for unpause button.
+               if (mouseY > (buttonY + 2.5) - buttonH / 2 && mouseY < (buttonY + 2.5) + buttonH / 2)
+                  break; // break the loop to end the method and resume the game
+
+               // this if checks the coordinates for restart button.
+               else if (mouseY > (buttonY) - buttonH / 2 && mouseY < (buttonY) + buttonH / 2){
+                  restartGame();
+               }
+
+               // this if checks the coordinates for exit button.
+               else if(mouseY > (buttonY - 2.5) - buttonH / 2 && mouseY < (buttonY - 2.5) + buttonH / 2){
+                  System.exit(0); // exit the application.
+               }
+         }
+
+         // the pause menu can also be closed by pressing escape
+         else if (StdDraw.isKeyPressed(KeyEvent.VK_ESCAPE)){
+            StdDraw.pause(5);
+            break;
+         }
+      }
+   }
+
+   // restartGame method recalls the main method without loading the intro menu.
+   public static void restartGame() throws InterruptedException {
+      isRestarted = true;
+      main(null);
    }
 }
